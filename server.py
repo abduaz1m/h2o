e74c71353d5b1516b2cf9e036f48fd4e5ce20073
Abduazim <abduazim002@gmail.com>
2025-12-13 08:35:06 +0500

    Fix home route response message

--- server.py
+++ server.py
@@ -2,9 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Crypto Bot is running!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    return "Bot is running!"
